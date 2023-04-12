245e68dd769817edc397fc1a276ba0c62d307b87
Axel Fuzzi <axelfuzziii@gmail.com>
2023-04-12 15:19:22 -0300

    en proceso

--- src/routes/product.js
+++ src/routes/product.js
@@ -1,75 +1,30 @@
 import express from "express";
-import { ProductoDao } from "../dao/ProductoDao.js";
 import { authMiddleware } from "../middlewares/Auth.js";
+import productController from "../controllers/product.controller.js";
 const router = express.Router();
-const productoDao = new ProductoDao();
-import logger from "../loggers/Log4jsLogger.js";
+
 
 // GET api/productos
 
-//router.get('/', async (_req, res) => {
-//    const products = await productoDao.getAll();
-//    products
-//        ? res.status(200).json(products)
-//        : res.status(400).json({"error": "there was a problem when trying to get the products"})
-//    
-//})
-
-
-
-router.get('/', async (_req, res)=>{
-    const products = await productoDao.getAll();
-    res.send(products); 
-})
-
+router.get('/', productController.getProducts);
 
 
 // GET api/productos/:id
 
-router.get('/:id', async(req, res) => {
-    const { id } = req.params;
-    const product = await productoDao.getProductById(id);
-    
-    product
-        ? res.status(200).json(product)
-        : res.status(400).json({"error": "product not found"})
-    
-})
+router.get('/:id', productController.getProductId);
 // 
 
 // POST api/productos
-router.post('/', authMiddleware, async (req,res) => {
-    const { body } = req;
-    const newProduct = await productoDao.createProduct(body);
-    
-    newProduct
-        ? res.status(200).json({"success": "Product added with ID " + newProduct._id})
-        : res.status(400).json({"error": "there was an error, please verify the body content match the schema"})
-    
-})
+router.post('/', authMiddleware, productController.postProduct);
+
 
 // PUT api/productos/:id
-router.put('/:id', authMiddleware, async (req,res) => {
-    const { id } = req.params;
-    const { body } = req;
-    const wasUpdated = await productoDao.updateProductById(id, body);
-    
-    wasUpdated
-        ? res.status(200).json({"success" : "product updated"})
-        : res.status(404).json({"error": "product not found or invalid body content."}) 
-})
+router.put('/:id', authMiddleware, productController.putProduct);
 
 
 // DELETE /api/productos/id
 
-router.delete('/:id', authMiddleware, async (req,res) => {
-    const { id } = req.params;
-    const wasDeleted = await productoDao.deleteProductById(id)
-
-    wasDeleted 
-        ? res.status(200).json({"success": "product successfully removed"})
-        : res.status(404).json({"error": "product not found"})
-})
+router.delete('/:id', productController.deleteProduct);
 
 
 
